16ded1db57ec621ba6d74450cd37c675eb8c91c5
MasterPrem001 <123372544+MasterPrem001@users.noreply.github.com>
2025-02-27 11:38:51 +0530

    Update prank.py

--- prank.py
+++ prank.py
@@ -1,19 +1,1 @@
-import pickle
-file = "conversation_history.pkl"
 
-def load_convo():
-    try:
-        with open(file, 'rb') as f:
-            return pickle.load(f)
-    except (FileNotFoundError, EOFError):
-        return []
-
-def save_convo(history):
-    with open(file, 'wb') as f:
-        pickle.dump(history, f)
-
-conversation = load_convo()
-user_input="So, there's this girl I'm chatting with. Do you think she loves me?"
-reply="🗣 Koi tumse kyu pyar karega? "
-conversation.append({'user': user_input, 'bot': reply})
-save_convo(conversation)
